5b866f3642df27edc80edcab3d4e184dce560da9
Yerko Henriquez R <yerko.henz@gmail.com>
2025-12-01 19:41:05 -0300

    feat: i18n setup

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/next";
 import CookieConsent from "@/components/Cookies";
-import { GoogleAnalytics } from '@next/third-parties/google'
-
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { NextIntlClientProvider } from "next-intl";
 
 export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_PRODUCTNAME,
@@ -15,22 +15,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  let theme = process.env.NEXT_PUBLIC_THEME
-  if(!theme) {
-    theme = "theme-sass3"
+  let theme = process.env.NEXT_PUBLIC_THEME;
+  if (!theme) {
+    theme = "theme-sass3";
   }
   const gaID = process.env.NEXT_PUBLIC_GOOGLE_TAG;
   return (
     <html lang="en">
-    <body className={theme}>
-      {children}
-      <Analytics />
-      <CookieConsent />
-      { gaID && (
-          <GoogleAnalytics gaId={gaID}/>
-      )}
-
-    </body>
+      <body className={theme}>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>{" "}
+        <Analytics />
+        <CookieConsent />
+        {gaID && <GoogleAnalytics gaId={gaID} />}
+      </body>
     </html>
   );
 }
