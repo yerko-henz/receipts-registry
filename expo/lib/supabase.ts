d7bf4c54b6c092a2079bdc801b7fa8a5afd271ed
Yerko Henriquez R <yerko.henz@gmail.com>
2026-01-16 16:56:14 -0300

    feat: update db types

--- expo/lib/supabase.ts
+++ expo/lib/supabase.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import 'react-native-url-polyfill/auto'
-import { Database } from './types'
+import { Database } from '../services/database.types'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
